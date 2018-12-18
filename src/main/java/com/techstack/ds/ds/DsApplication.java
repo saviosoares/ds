0ca475df2386da3b4e93cdb181c8e687bc938dd0
saviosoares <savio.r@thoughtworks.com>
2018-12-18 17:23:30 -0300

    first commit

---
+++ src/main/java/com/techstack/ds/ds/DsApplication.java
@@ -0,0 +1,14 @@
+package com.techstack.ds.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsApplication.class, args);
+	}
+
+}
+
